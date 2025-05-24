5d8022c5f996e59ab24160a9dd918efd45da828f
Lina Salazar <salazarlina35@gmail.com>
2025-05-24 12:28:40 -0500

    Pagina Detalle

---
+++ src/app/componentes/resenia/resenia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resenia',
+  imports: [],
+  templateUrl: './resenia.component.html',
+  styleUrl: './resenia.component.scss'
+})
+export class ReseniaComponent {
+
+}
