31a64c1d5255d0ce0e979c767ec38eba109aa8d8
natabarahona <baranata237@gmail.com>
2025-05-24 13:06:36 -0500

    Footer y pagina contacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavegacionComponent],
+  imports: [RouterOutlet, NavegacionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
+    {path:'contacto', component: ContactoComponent}
 ];
