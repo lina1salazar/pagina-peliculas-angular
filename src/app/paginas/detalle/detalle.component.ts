1e3da3574b0eac0f94ade40ec2c1bfed51e97072
Lina Salazar <salazarlina35@gmail.com>
2025-08-23 17:07:18 -0500

    Ajustar el footer y eliminar reseñas

--- src/app/paginas/detalle/detalle.component.ts
+++ src/app/paginas/detalle/detalle.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ComentariosComponent } from '../../componentes/comentarios/comentarios.component';
-import { ReseniasComponent } from '../../componentes/resenias/resenias.component';
 import { DetallePeliculaComponent } from '../../componentes/detalle-pelicula/detalle-pelicula.component';
 import { OtrasPeliculasComponent  } from '../../componentes/otras-peliculas/otras-peliculas.component';
 
 @Component({
   selector: 'app-detalle',
   standalone: true,
-  imports: [DetallePeliculaComponent, ComentariosComponent, ReseniasComponent, OtrasPeliculasComponent ],
+  imports: [DetallePeliculaComponent, ComentariosComponent, OtrasPeliculasComponent ],
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.scss']
 })
